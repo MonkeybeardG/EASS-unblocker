67611babcffe97d4a5f89cb30c8c31429f805dc3
Carter <carterhemsley1@hotmail.com>
2024-11-23 22:04:49 -0800

    pretty much created

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  challenge: true, // Set to true if you want to enable password protection.
+  blocked: {
+    // Normal users can ignore this option. This is for the owner of the website.
+    "gointerstellar.app": "",
+  },
+  users: {
+    // You can add multiple users by doing username: 'password'.
+    chem: "6481",
+  },
+};
+
+export default config;
